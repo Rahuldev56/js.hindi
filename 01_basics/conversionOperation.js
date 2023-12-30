d6fb9ebb3fce9a748bc7c38b3b16ab684980f475
Rahuldev56 <100990357+Rahuldev56@users.noreply.github.com>
2023-12-30 13:35:14 +0000

    conversion.of data types

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,17 @@
+let score=null
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+//conversion 
+// "33"=>33 String to number
+//"33abc"=>Nan// String to number
+ let loggedIn=0;
+ let booleanLoggedIn=Boolean(loggedIn);
+ console.log(booleanLoggedIn);
+ console.log(typeof booleanLoggedIn);
+
+
+ //0=> false ;1=> true
+ //""=>false
+ //"rahul"=> true;
