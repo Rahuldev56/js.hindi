fcec57087158216a9a44dabd85e317b7fd3d5b2f
Rahuldev56 <100990357+Rahuldev56@users.noreply.github.com>
2023-12-30 13:02:50 +0000

    datatype

---
+++ 01_basics/01_datatTypes.js
@@ -0,0 +1,10 @@
+let name="Rahul"
+let age=18
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+// null=>standalone value
+//undefined=>
+//symbol=>unique
---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="rahul@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+//accountId=2// not allowed
+accountEmail="hac.com"
+accountPassword="21212121"
+accountCity="Bangluru"
+console.table([accountId,accountEmail,accountPassword,accountCity])
+{
+    let a=20;
+    // this gives error because we are trying to access outside of the scope.
+}
+console.log(a);
