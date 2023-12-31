8f5405fe7ea513e777710c4d3805a3f022e74642
Rahuldev56 <100990357+Rahuldev56@users.noreply.github.com>
2023-12-31 12:30:44 +0000

    string and methods

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const name="rahul"
 const repoCount=50
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
@@ -12,3 +13,20 @@ const anotherString= gameName.slice(2,-5);
 console.log(anotherString);
 console.log(gameName.replace('r','h'));
 console.log(gameName.split(' '));
+=======
+const name="rahul"
+const repoCount=50
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+ const  gameName=new String('rahul dev')
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ // charAt method is used to know what character is present at given index.
+ console.log(gameName.indexOf('u'));
+//index of method is used to know the position of chararcter in the specific string.
+const anotherString= gameName.slice(2,-5);
+console.log(anotherString);
+console.log(gameName.replace('r','h'));
+console.log(gameName.split(' '));
+
+>>>>>>> 2e573db (string and methods)
