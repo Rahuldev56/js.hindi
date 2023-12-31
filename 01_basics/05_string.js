f8fb3adbf2978933acb1866b00d7d376d7bd69fd
Rahuldev56 <100990357+Rahuldev56@users.noreply.github.com>
2023-12-31 18:15:44 +0530

    Update 05_string.js
    string and methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,14 @@
+const name="rahul"
+const repoCount=50
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+ const  gameName=new String('rahul dev')
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName.charAt(2));
+ // charAt method is used to know what character is present at given index.
+ console.log(gameName.indexOf('u'));
+//index of method is used to know the position of chararcter in the specific string.
+const anotherString= gameName.slice(2,-5);
+console.log(anotherString);
+console.log(gameName.replace('r','h'));
+console.log(gameName.split(' '));
