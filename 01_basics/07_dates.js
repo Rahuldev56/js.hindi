0bfa4c27484b89b23313ddf2b5ed9b58d9e0fc0c
Rahuldev56 <100990357+Rahuldev56@users.noreply.github.com>
2024-01-04 09:48:34 +0000

    Dates and Time  in javaScript

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,6 @@
+let myDate=new Date();
+console.log(myDate.toString());
+let myCreatedDate=new Date(2023,13,24);
+console.log(myCreatedDate.toDateString());
+let myTimeStamp=Date.now();
+console.log(myTimeStamp);
