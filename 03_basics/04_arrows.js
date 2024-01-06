653296d4fded4ef8b7d5b2ab4d55f00144efe256
Rahuldev56 <100990357+Rahuldev56@users.noreply.github.com>
2024-01-06 14:37:03 +0000

    arrow and this in javaScript

---
+++ 03_basics/04_arrows.js
@@ -0,0 +1,26 @@
+const user={
+    username:"Rahul",
+    price:999,
+    welcomeMessage:function(){
+        console.log(`${this.username},welcome to website`)
+    }
+
+}
+// this refers to current context of objects  i.e variables etc.
+user.welcomeMessage();
+user.username="sam"
+user.welcomeMessage()
+// arrow function in javaScript
+const ans= () => {
+    let username="hitesh"
+    console.log(this.username)
+
+}
+ans();
+//const addTwo = (num1,num2) => {
+//return num1+num2
+// we have to use return keyword with curly braces.
+//}
+//another way of definning arrow function hint: useful in objects
+const addTwo=(num1,num2)=>(num1+num2)
+console.log(addTwo(3,8))
