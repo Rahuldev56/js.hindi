cbee4d01e0e535442d1f19850327a2c14eb461f2
Rahuldev56 <100990357+Rahuldev56@users.noreply.github.com>
2024-01-08 07:02:08 +0000

    loops inn javaScript

---
+++ 05_iterations/one.js
@@ -0,0 +1,25 @@
+// for loop in javaScript.
+
+for(let index=0;index<10;index++){
+    const element=index;
+console.log(element);
+}
+// nested loop used for mutiplication table.
+for(let i=1;i<=10;i++){
+    
+    for(let j=1;j<=10;j++){
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+    console.log("\n")
+}
+ // while loop.
+ let i=1;
+ while(i<5){
+    console.log(i);
+    i++;
+ }
+// do while loop
+do{
+    console.log(i)
+    i++
+} while(i<10)
